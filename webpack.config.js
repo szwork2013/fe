3ee3c51fd300ba926649d88b98cb6837393fd084
Jan Zelenka <jan.zelenka@expobank.cz>
2015-08-05 09:33:17 +0200

    dev

---
+++ webpack.config.js
@@ -0,0 +1,48 @@
+'use strict';
+
+var webpack = require('webpack'),
+    HtmlWebpackPlugin = require('html-webpack-plugin'),
+    path = require('path'),
+    srcPath = path.join(__dirname, 'app');
+
+module.exports = {
+    target: 'web',
+    cache: true,
+    entry: {
+        module: path.join(srcPath, 'module.js'),
+        common: ['react', 'react-router', 'alt']
+    },
+    resolve: {
+        root: srcPath,
+        extensions: ['', '.js'],
+        modulesDirectories: ['node_modules', 'app']
+    },
+    output: {
+        path: path.join(__dirname, 'build'),
+        publicPath: '',
+        filename: '[name].js',
+        library: ['Zauzoo', '[name]'],
+        pathInfo: true
+    },
+
+    module: {
+        loaders: [
+            {test: /\.js?$/, exclude: /node_modules/, loader: 'babel?cacheDirectory'}
+        ]
+    },
+    plugins: [
+        new webpack.optimize.CommonsChunkPlugin('common', 'common.js'),
+        new HtmlWebpackPlugin({
+            inject: true,
+            template: 'app/index.html'
+        }),
+        new webpack.NoErrorsPlugin()
+    ],
+
+    debug: true,
+    devtool: 'eval-cheap-module-source-map',
+    devServer: {
+        contentBase: './build',
+        historyApiFallback: true
+    }
+};
