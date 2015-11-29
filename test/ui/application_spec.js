02b6c752ee27a4c2fc044e0f9ebe6973b94def73
Jan Zelenka <zelenka.jan@gmail.com>
2015-11-29 12:04:31 +0100

    mocha tests config

--- test/ui/application_spec.js
+++ test/ui/application_spec.js
@@ -1,19 +1,22 @@
+import {expect} from 'chai';
 import React from 'react';
 import {renderIntoDocument, scryRenderedDOMComponentsWithTag} from 'react-addons-test-utils';
 
 import Application from 'core/application/application';
+import stubRouterContext from '../stubRouterContext';
 
 
 
 describe('Application', () => {
 
   it('application is running', () => {
+    var Subject = stubRouterContext(Application, {someProp: 'foo'});
     const component = renderIntoDocument(
-        <Application/>
+        <Subject/>
     );
 
     const divs = scryRenderedDOMComponentsWithTag(component, 'div');
-
+    
     expect(divs.length).to.be.above(0);
 
   });
